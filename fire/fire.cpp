23055c15a05225369b9a6b8ba992f50e99f4d25d
Jon Rafkind <jon@rafkind.com>
2021-01-10 11:33:51 -0800

    fire: make simulation use a timer, increase fire width/height

--- fire/fire.cpp
+++ fire/fire.cpp
@@ -3,14 +3,21 @@
 #include <stdlib.h>
 #include <time.h>
 
-BITMAP *work;
-
-const int max_y = 60;
+const int max_y = 180;
 const int what = ( 480 - max_y );
+const int spot_width = 20;
+
+#define MAX_SPOTS 20
 
 unsigned char back[640][max_y];
 unsigned char real[640][max_y];
-unsigned int spot[20];
+unsigned int spot[MAX_SPOTS];
+
+unsigned int grand_clock;
+
+void inc_timer(){
+	grand_clock++;
+}
 
 void init() {
         allegro_init();
@@ -19,9 +26,7 @@ void init() {
         install_keyboard();
         set_color_depth( 8 );
         set_gfx_mode( GFX_AUTODETECT_WINDOWED, 640, 480, 640, 480 );
-
-        text_mode( 0 );
-
+        install_int_ex( inc_timer, MSEC_TO_TIMER( 8 ) );
 }
 
 
@@ -31,21 +36,21 @@ void set_pal() {
 
         int t;
 
-        for (t=0; t<63; t++) {
+        for (t=0; t<64; t++) {
 
                 pal[t].r = t;
                 pal[t].g = 0;
                 pal[t].b = 0;
 
         }
-        for (t=0; t<63; t++) {
+        for (t=0; t<64; t++) {
 
-                pal[t+63].r = 63;
-                pal[t+63].g = t;
-                pal[t+63].b = 0;
+                pal[t+64].r = 63;
+                pal[t+64].g = t;
+                pal[t+64].b = 0;
 
         }
-        for (t=0; t<63; t++) {
+        for (t=0; t<64; t++) {
 
                 pal[t+128].r = 63;
                 pal[t+128].g = 63;
@@ -65,7 +70,7 @@ void set_pal() {
 
         set_palette( pal );
 
-        for (int q=0; q<20; q++) {
+        for (int q=0; q<MAX_SPOTS; q++) {
 
                 spot[ q ] = random() % 640;
 
@@ -76,36 +81,43 @@ void set_pal() {
 
 void draw_back() {
 
-        for ( int x = 0; x < 640; x++ )
-                for ( int y = 0; y < max_y; y++ )
-                        real[x][y] = 0;
+    for ( int x = 0; x < 640; x++ )
+        for ( int y = 0; y < max_y; y++ )
+            real[x][y] = 0;
 
-        for (int t =0; t<640; t++)
-                back[t][max_y-1] = 0;
+    for (int t =0; t<640; t++)
+        back[t][max_y-1] = 0;
 
-        for( int t=0; t<20; t++) {
-            if (spot[t] > 0 && spot[t] < 640){
-                back[spot[t]][max_y-1] = 192;
-            }
-
-                for (int w =-5; w<5; w++){
-                    int x_pos = spot[t] + w;
-                    if (x_pos > 0 && x_pos < 640){
-                        back[x_pos][max_y-1] = 192- abs( w );
-                    }
-                }
-
+    for( int t=0; t<MAX_SPOTS; t++) {
+        if (spot[t] > 0 && spot[t] < 640){
+            back[spot[t]][max_y-1] = 192;
         }
 
-        for (int t =0; t<20; t++)
-                spot[t] = spot[t] + random() % 5 - 2;
+        for (int w =-spot_width/2; w<spot_width/2; w++){
+            int x_pos = spot[t] + w;
+            if (x_pos > 0 && x_pos < 640){
+                back[x_pos][max_y-1] = 192 - abs( w );
+            }
+        }
+
+    }
+
+    for (int t =0; t<MAX_SPOTS; t++){
+        spot[t] = spot[t] + random() % 5 - 2;
+        if (spot[t] < 0){
+            spot[t] = 639;
+        }
+        if (spot[t] > 639){
+            spot[t] = 0;
+        }
+    }
 
 }
 
 
-void show_it() {
+void show_it(BITMAP* work) {
 
-        const int dec = 7;
+        const int dec = (int) (192.0 / (float) max_y + 0.5);
 
         for (int y = 0; y<max_y-1; y++) {
 
@@ -133,6 +145,9 @@ void show_it() {
 
         }
 
+        // for debugging
+        // hline(work, 0, 0, 640, 191);
+
 }
 
 
@@ -150,22 +165,32 @@ void copy_it() {
 
 
 int main() {
+    init();
+    set_pal();
+    BITMAP * work = create_bitmap(640, max_y);
 
-        init();
+    for (int i = 0; i < 40; i++){
+        draw_back();
+        show_it(work);
+        copy_it();
+    }
 
-        set_pal();
-
-        work = create_bitmap( 640, max_y );
-
-        while (!key[ KEY_ESC]) {
-                draw_back();
-                show_it();
-                copy_it();
-
-                blit( work, screen, 0, 0, 0, what, 640, max_y-1 );
+    while (!key[ KEY_ESC]) {
+        while (grand_clock == 0){
+            rest( 1 );
         }
 
-        destroy_bitmap( work );
+        while ( grand_clock > 0 ){
+            grand_clock--;
+            draw_back();
+        }
 
+        show_it(work);
+        copy_it();
+
+        blit( work, screen, 0, 0, 0, what, 640, max_y-1 );
+    }
+
+    destroy_bitmap(work);
 }
 END_OF_MAIN();
